d2ba7effce38ee4f2f923de74713faa89870abb8
kalin <kalin@funktroniclabs.com>
2020-09-17 09:43:36 -0700

    export test wip

---
+++ CroppyEditor/Assets/Scripts/Editor/ExportWorld.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Content;
+
+public class ExportWorld
+    : MonoBehaviour
+{
+    [MenuItem("Export/Export World")]
+    public static void MenuExportWorld()
+    {
+        var scene = UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene();
+        var path = Path.GetFullPath(Path.Combine(Application.dataPath, "../bundles"));
+
+        Debug.LogFormat("Export: exporting to {0}...", path);
+
+        if (Directory.Exists(path) == false)
+            Directory.CreateDirectory(path);
+
+        // go through each proxy, store json representation
+        BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.AssetBundleStripUnityVersion, BuildTarget.StandaloneWindows);
+    }
+}
