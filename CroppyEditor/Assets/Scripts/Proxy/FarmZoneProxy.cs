6f2d04cd40e5b16e430c3286e5efe97abef687aa
kalin <kalin@funktroniclabs.com>
2020-09-26 12:51:10 -0700

    validation checks

--- CroppyEditor/Assets/Scripts/Proxy/FarmZoneProxy.cs
+++ CroppyEditor/Assets/Scripts/Proxy/FarmZoneProxy.cs
@@ -6,4 +6,16 @@ public class FarmZoneProxy
     public ClaimBoardProxy claimBoardProxy;
     public ShippingBoxProxy shippingBoxProxy;
     //public FarmGateProxy farmGateProxy;
+
+#if UNITY_EDITOR
+    public void OnValidate()
+    {
+        if (GetComponent<Collider2D>() == null)
+            Debug.LogErrorFormat(gameObject, "FarmZone: missing Collider2D");
+        if (claimBoardProxy == null)
+            Debug.LogWarningFormat(gameObject, "FarmZone: maybe missing claim board?");
+        if (shippingBoxProxy == null)
+            Debug.LogWarningFormat(gameObject, "FarmZone: maybe missing shipping box?");
+    }
+#endif
 }
